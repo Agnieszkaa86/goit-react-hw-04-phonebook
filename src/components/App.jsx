7b464d119f08a122b531748fa51e78969854bd7b
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-18 11:09:51 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,38 +5,36 @@ import { Filter } from './Filter/Filter';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Container } from './ContactForm/ContactForm.styled';
 
-const LOKAL_KEY = 'contacts';
+const CONTACTS_STORAGE_KEY = 'contacts';
 
 export const App = () => {
-  const initiateContacts = () =>
-    JSON.parse(localStorage.getItem(LOKAL_KEY)) || [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ];
 
-  const [filter, setFilter] = useState('');
-  const [contacts, setContacts] = useState(initiateContacts);
+   const [contacts, setContacts] = useState(()=>JSON.parse(localStorage.getItem(CONTACTS_STORAGE_KEY))|| []);
+   const [filter, setFilter] = useState('');
   useEffect(() => {
-    try {
-      const initiateContacts = JSON.stringify(contacts);
-      localStorage.setItem('LOKAL_KEY', initiateContacts);
-    } catch (error) {
-      console.error('Set state error: ', error.message);
-    }
+    const contactsJSON = JSON.stringify(contacts);
+    localStorage.setItem(CONTACTS_STORAGE_KEY, contactsJSON);
   }, [contacts]);
+   
 
   const addNewContact = ({ name, number }) => {
-    const foundContact = contacts.find(contact =>
-      contact.name.toLowerCase().includes(name.toLowerCase())
-    );
-    if (foundContact) {
-      alert(`${foundContact.name} is already in contacts`);
+    if (contacts.find(contact =>
+      contact.name===name)) {
+      alert(`${name} is already in contacts`);
       return;
-    } else {
-      setContacts([...contacts, { name, number, id: nanoid() }]);
     }
+    if (contacts.find(contact => contact.number === number)){
+      alert(`Number ${number} already exists`);
+      return;
+    }
+    const newContact = {
+      name,
+      number,
+      id: nanoid()
+    };
+    
+      setContacts([...contacts,newContact]);
+    
   };
 
   const searchByName = event => {
